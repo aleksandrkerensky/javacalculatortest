8c3f6c62833deb154a56f8f30bb1fd593b8aff21
Paul Zurita <paulzuri@tutanota.com>
2023-03-23 22:12:36 -0500

    Implemented the backend

--- src/backend/NumberAnalyzer.java
+++ src/backend/NumberAnalyzer.java
@@ -4,10 +4,87 @@
  */
 package backend;
 
+import java.text.DecimalFormat;
+
 /**
+ * Backend class for a calculator
+ *
+ * To be used with GitHub, includes essential operations
  *
  * @author paulz
  */
 public class NumberAnalyzer {
-    
+
+    public NumberAnalyzer() {
+    }
+
+    /**
+     * Casts and adds two numbers fetched from a JTextField, outputs a double
+     *
+     * Receives two numbers from a JTextField, casts them to doubles and
+     * performs an addition, the resulting number is another double
+     *
+     * @param firstNumber first number to add
+     * @param secondNumber second number to add
+     * @return the result, as a double
+     */
+    public double add(String firstNumber, String secondNumber) {
+        return Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+    }
+
+    /**
+     * Casts and subtracts two numbers fetched from a JTextField, outputs a
+     * double
+     *
+     * Receives two Strings, casts them to doubles and performs a subtraction
+     *
+     * @param firstNumber minuend
+     * @param secondNumber subtrahend
+     * @return the result, as a double
+     */
+    public double subtract(String firstNumber, String secondNumber) {
+        return Double.parseDouble(firstNumber) - Double.parseDouble(secondNumber);
+    }
+
+    /**
+     * Casts and multiplies two numbers fetched from a JTextField, outputs a
+     * double
+     *
+     * Receives two Strings, casts them to doubles and performs a multiplication
+     *
+     * @param firstNumber multiplicand
+     * @param secondNumber markiplier
+     * @return the result, as a double
+     */
+    public double multiply(String firstNumber, String secondNumber) {
+        return Double.parseDouble(firstNumber) * Double.parseDouble(secondNumber);
+    }
+
+    /**
+     * Casts and divides two numbers fetched from a JTextField, outputs a double
+     *
+     * Receives two Strings, casts them to doubles and performs a division
+     *
+     * @param firstNumber dividend
+     * @param secondNumber divisor
+     * @return the result, as a double
+     */
+    public double divide(String firstNumber, String secondNumber) {
+        return Double.parseDouble(firstNumber) / Double.parseDouble(secondNumber);
+    }
+
+    /**
+     * Two decimals format
+     *
+     * Receives a double and outputs a String with the decimal format "0.00"
+     *
+     * @param number number to be formatted
+     *
+     * @return the resulting formatted number, as a String
+     */
+    public String printToTwoDecimals(Double number) {
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        return df.format(number);
+    }
 }
